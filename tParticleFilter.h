3125c44e424f4428d8ba9d4af8f5f754b7b12169
Tobias Föhst <foehst@informatik.uni-kl.de>
2011-10-07 17:54:04 +0200

    Updated and added old/missing copyright notes

--- tParticleFilter.h
+++ tParticleFilter.h
@@ -2,7 +2,7 @@
 // You received this file as part of RRLib
 // Robotics Research Library
 //
-// Copyright (C) AG Robotersysteme TU Kaiserslautern
+// Copyright (C) Finroc GbR (finroc.org)
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -86,7 +86,7 @@ public:
     double score;
 
     inline tParticle(const tConfiguration &configuration, double score)
-    : configuration(configuration), score(score)
+        : configuration(configuration), score(score)
     {}
 
   public:
